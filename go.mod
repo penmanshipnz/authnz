200088a28700b231ecb6573bf54bc0e33e820ebc
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-01-06 16:22:24 +1300

    chore: basic setup

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module example/hello
+module penmanship/authnz
 
 go 1.21.5
+
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+	github.com/gorilla/csrf v1.7.2
+)
+
+require github.com/gorilla/securecookie v1.1.2 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/gorilla/csrf"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
-}
+	env := os.Getenv("GO_ENV")
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	r := chi.NewRouter()
+
+	r.Use(middleware.RedirectSlashes)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.AllowContentType("application/json"))
+	r.Use(middleware.Compress(5))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+	}))
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root."))
+	})
+
+	// TODO: CSRF stuff needs updating
+	// - Auth key
+	// - https://github.com/gorilla/csrf#javascript-applications
+	csrf.Secure(env == "production")
+	CSRF := csrf.Protect([]byte("32-byte-long-auth-key"))
+
+	http.ListenAndServe(":1001", CSRF(r))
 }
